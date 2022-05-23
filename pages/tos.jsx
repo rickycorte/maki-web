679397e78bed7687111a3a5bcbd5838f0bf24425
Riccardo <rickycortef@gmail.com>
2022-05-23 16:16:33 +0200

    Added tos page

---
+++ pages/tos.jsx
@@ -0,0 +1,29 @@
+import Container from "../components/container";
+import Footer from "../components/footer";
+import GradientTitle from "../components/grandietTitle";
+
+export default function ToSPage (){
+    return (
+        <Container>
+            <GradientTitle>
+                <a href="/" style={{width: "100%", textAlign: "center"}}>
+                    <h3 style={{width: "100%", textAlign: "center"}}>Maki</h3>
+                </a>
+            </GradientTitle>
+            <div className="container link">
+                <div className="container_body">
+                    <h1>Terms of Service</h1>
+                    <ol>
+                        <li>Free for commercial and non-commercial use.</li>
+                        <li>You must not try to bypass in any way our rate limit.</li>
+                        <li>You must not try to exploit Maki API.</li>
+                        <li>You must use "Maki" in the title/name of any of you products that uses Maki API.</li>
+                        <li>You are prohibited from use within competing noncomplementary services of the same nature, including but not limited to recommender systems.</li>
+                        <li>By using Maki API you agree to this terms and to our privacy policy.</li>
+                    </ol>
+                </div>
+            </div>
+            <Footer></Footer>
+        </Container>
+        )
+}
