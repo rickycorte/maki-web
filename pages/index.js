83b33c92bdba8468ea747b5f1dbc1bad625a2fb6
Riccardo <rickycortef@gmail.com>
2022-05-25 18:11:44 +0200

    Added main page

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,75 @@
 import React from 'react';
-import AnimeCard from '../components/animecard';
 import Container from '../components/container';
+import Footer from '../components/footer';
 import GradientTitle from '../components/grandietTitle';
-import Grid from '../components/grid';
+import {withRouter} from 'next/router'
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
     this.props = props
 
+    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.onSiteChange = this.onSiteChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state= {
+      username: "",
+      site: "anilist"
+    }
+  }
+
+  onUsernameChange(event) {
+    this.setState({username: event.target.value});
+  }
+
+  onSiteChange(event) {
+    this.setState({site: event.target.value});
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    if(this.state.username != null && this.state.username.length > 3)
+      this.props.router.push(`${this.state.site}/${this.state.username}`)
   }
 
   render() {
 
     return (
-      <Container><GradientTitle><h3 style={{height:"100%"}}>Hello there :3</h3></GradientTitle></Container>
+      <Container>
+        <div className="front">
+          <div className="search">
+            <GradientTitle>
+                <form onSubmit={this.onSubmit}>
+                  <div className='rowFull'>
+                      <div className='row'>
+                        <h3>Hi</h3>
+                        <input style={{marginLeft: "5px", width:"100%"}} placeholder="username" value={this.state.username} onChange={this.onUsernameChange}></input>
+                      </div>
+                      <div className='row'>
+                        <h3>from</h3>
+                        <select style={{marginLeft: "5px"}} value={this.state.site} onChange={this.onSiteChange}>
+                          <option value="anilist">Anilist</option>
+                          <option value="mal">MyAnimeList</option>
+                        </select>
+                      </div>
+                  </div>
+                  <button type="submit">Recommend me!</button>
+                </form>
+            </GradientTitle>
+          </div>
+        </div>
+        <div className="container up">
+                <div className="container_body hello">
+                    <h1>Hi sen(pi)~</h1>
+                    <h3>Welcome to Maki! The first fully automated recommender system for anime!</h3>
+                    <p>Maki is 100% free to use and integrate in any application by using its REST API.</p>
+                </div>
+            </div>
+        <Footer></Footer>
+        </Container>
     )
   }
 }
 
-export default HomePage
+export default withRouter(HomePage)
