04c1bab18f357c72c968e08627718ba9e2558a41
Riccardo <rickycortef@gmail.com>
2022-08-07 16:49:59 +0200

    Updated headings

--- pages/index.js
+++ pages/index.js
@@ -86,9 +86,10 @@ class HomePage extends React.Component {
             <Image src={chicaPic} alt="Chika Fujiwara from Love is War"/>
           </div>
           <div className="column is-centered hello">
-            <h1 className='is-size-1 is-center-mobile'>Welcome to Maki!</h1>
-            <p>  
-              Maki is a fully automated recommender system that is able to generate anime recommendations based on your anime list.
+            <h1 className='is-size-1 is-center-mobile'>Get your anime recommendations!</h1>
+            <p>
+              Tired of looking for anime recommendations on Reddit or in some other site? 
+              Maki is a fully automated recommender system that is able to generate as much anime recommendations as you want in just a couple of seconds.
             </p>
             <h2 className='is-size-4 mt-6'>What does this mean?</h2>
             <p>
@@ -102,7 +103,7 @@ class HomePage extends React.Component {
           </div>
         </div>
         <div className='darker'>
-          <div><h1 className='has-text-centered is-size-3'>ANIME DATABASE</h1></div>
+          <div><h2 className='has-text-centered is-size-3'>ANIME DATABASE</h2></div>
           <nav className="level">
             <div className="level-item has-text-centered">
               <div>
@@ -150,18 +151,18 @@ class HomePage extends React.Component {
           <div className="column is-centered is-vcentered">
             <Image src={confusedPic} alt="Anime girl surrounded by question marks"/>
           </div>
-          <div className="column is-centered hello  is-vcentered">
-            <h1 className='is-size-1 is-center-mobile'>How it works?</h1>
+          <div className="column is-centered hello is-vcentered">
+            <h2 className='is-size-1 is-center-mobile'>How it works?</h2>
             <p>  
               Maki is backed by a powerful algorithm that is updated every day with all the user lists available in the database.
               This means that you always get up-to-date and meaningful recommendations based on all the data available to Maki!
             </p>
-            <h2 className='is-size-4 mt-6'>Why I did not get the perfect recommendations?</h2>
+            <h3 className='is-size-4 mt-6'>Why I did not get the perfect recommendations?</h3>
             <p>
               Maki is a fully automated system that works without human supervision.
               It make take a while to learn how to give you proper recommendations and it will never be perfect, afterall its just an algorithm.
             </p>
-            <h2 className='is-size-4 mt-6'>What data does Maki use?</h2>
+            <h3 className='is-size-4 mt-6'>What data does Maki use?</h3>
             <p>
               Maki collects and uses only public user lists. No other data is collected and used to generate recommendations.
             </p>
@@ -225,15 +226,15 @@ class HomePage extends React.Component {
             <Image src={programmingPic} alt="Anime girl holding a laptop and a C book"/>
           </div>
           <div className="column is-centered hello is-vcentered">
-            <h1 className='is-size-1 is-center-mobile'>Maki for developers</h1>
+            <h2 className='is-size-1 is-center-mobile'>Maki for developers</h2>
             <p>  
               Maki is the only anime recommender system that offers a free REST API! If you are intrested to use Maki in your project checkout the documentation!
             </p>
-            <h2 className='is-size-4 mt-6'>Why do you offer a free API?</h2>
+            <h3 className='is-size-4 mt-6'>Why do you offer a free API?</h3>
             <p>
               To allow developer to improve and offer new features in their own anime-related projects!
             </p>
-            <h2 className='is-size-4 mt-6'>Where is the catch?</h2>
+            <h3 className='is-size-4 mt-6'>Where is the catch?</h3>
             <p>
               Maki is not monetized and to keep costs low it runs on low end hardware, it may slow down on heavy load.
             </p>
